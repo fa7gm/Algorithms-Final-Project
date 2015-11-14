64007bf2dccd4a5493e9ec8e76136c94c1400e8e
AmberJMLee <alphagirl93@yahoo.com>
2015-11-13 20:21:09 -0500

    Image successfully renders.

--- Voronoi Python/VoronoiMain.py
+++ Voronoi Python/VoronoiMain.py
@@ -2,6 +2,8 @@ __author__ = 'Amy Shi, Amber Lee, Fred Ayi-Quaye'
 
 from tkinter import *
 import math
+from PIL import Image
+import operator
 
 class Context:
     def __init__(self):
@@ -31,22 +33,41 @@ def angle(pt1,pt2):
 context = Context()
 
 #For reading coordinates from a text file
-with open('points.txt') as f:
-    w, h = [int(x) for x in f.readline().split()]
-    array = [[int(x) for x in line.split()] for line in f]
-for p in array:
-    print(p)
+#with open('points.txt') as f:
+#    w, h = [int(x) for x in f.readline().split()]
+#    array = [[int(x) for x in line.split()] for line in f]
+#for p in array:
+#    print(p)
 
 
 #Graphics (Currently just a sample at the moment.)
 root = Tk()
 w = Canvas(root, width=200, height=100)
 w.pack()
-w.create_line(0,0,200,100)
-w.create_line(0, 100, 200, 0, fill="red", dash=(4,4))
-w.create_rectangle(50,25,150,75,fill="blue")
-#This actually works. It's just very small.
-w.create_polygon(array[0][0], array[0][1], array[1][0], array[1][1], array[2][0], array[2][1], array[3][0], array[3][1], fill="blue")
+#w.create_line(0,0,200,100)
+#w.create_line(0, 100, 200, 0, fill="red", dash=(4,4))
+#w.create_rectangle(50,25,150,75,fill="blue")
+
+
+#Let's create an image
+image = Image.open('sample.png')
+pixels = image.load()
+width, height = image.size
+all_pixels = []
+for x in range(width):
+    for y in range(height):
+        cpixel = pixels[x, y]
+        if round(sum(cpixel)) / float(len(cpixel)) > 127:
+            all_pixels.append(255)
+            w.create_oval(x, y, x+1, y+1, fill="white")
+            print(x, ", ", y, " white")
+
+        else:
+            print(x, ", ", y, " Black")
+           # w.create_oval(x, y, x + 1, y + 1, fill="white")
+            all_pixels.append(0)
+
+print(all_pixels.__len__())
 root.title("Voronoi Painting")
 root.geometry("500x500")
 
