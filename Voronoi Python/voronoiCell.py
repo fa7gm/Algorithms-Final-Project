20af9b6542e79cb6d371150b8b039f7907eca2aa
amyshi188 <ds2ca@virginia.edu>
2015-12-10 03:04:16 -0500

    classes Cell, Point, and Line

---
+++ Voronoi Python/voronoiCell.py
@@ -0,0 +1,34 @@
+__author__ = 'Amy Shi, Amber Lee, Fred Ayi-Quaye'
+
+from tkinter import *
+import random
+import math
+import time
+import doctest
+from itertools import permutations
+from PIL import Image
+import VoronoiMain.py
+
+# A point has an x and a y coordinate
+# x and y must be numbers
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+# A line has a start point and an end point
+# The start and end points are of class Point
+class Line:
+	def __init__(self, start, end):
+		self.start = start
+		self.end = end
+
+# A cell has a center, adjacent cells, and walls
+# The center is a Point object
+# The adjacent cells list is a list of cells
+# The walls are of class Line
+class Cell:
+	def __init__(self, center, adjacent, walls):
+		self.center = center
+		self.adjacent = adjacent
+		self.walls = walls
